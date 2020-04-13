b8e8b3decc8aaa90aef03a7c2240869862c58488
And <st4nny@gmail.com>
2020-04-13 23:17:22 +0200

    up

--- execute.sh
+++ execute.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#10 *    * * *   root    . /home/user/esselounge-master/execute.sh
+# ogni dieci minuti
+# */10 * * * *  root    . /home/user/esselounge-master/execute.sh
 node index.js
